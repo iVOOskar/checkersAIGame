855d6dfb49a99d800e0f2036e9b2e5f2a44f357b
oh210602 <OH210602@reigate.ac.uk>
2022-06-08 13:28:32 +0100

    Initial commit

---
+++ src/main/java/com/example/checkersaigame/MainController.java
@@ -0,0 +1,12 @@
+package com.example.checkersaigame;
+
+import java.awt.*;
+
+public class MainController {
+    @FXML
+    private Label lblStatus;
+    @FXML
+    private Label lblStatus;
+    @FXML
+    private Label lblStatus;//6:34 create login application for beginers 10
+}
